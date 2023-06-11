6e2abf7b9d5a01c4c4d38730f3f5ef827f57785c
Noman1103 <130471196+Noman1103@users.noreply.github.com>
2023-06-12 01:19:13 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MangaVerse</title>
+    <link rel="stylesheet" href="css/mangas.css">
+</head>
+
+<header class="navbar">
+
+    <a href="index.php" class="logo_nav">
+        <img src="media/img/torii-gate.png" width='50px'>
+    </a>
+
+    <nav class='nav_bar'>
+        <a class="nav_link" href="index.php">Accueil</a>
+        <a class="nav_link" href="manga.php">Manga</a>
+        <a class="nav_link" href="#">Actualité</a>
+        <a class="nav_link" href="#">Shopping</a>
+        <a class="nav_link" href="#">Chat</a>
+    </nav>
+
+    <a href="login.php" class="login">Connexion</a>
+
+</header>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+include 'header.php'; 
+include 'connect.php';
+
+    $req = "SELECT * from manga limit 4";
+    $resultat = mysqli_query($id, $req);
+?>
+
+<body>
+    <div class="slider-container slider-1">
+        <div class="slider">
+            <img src="media/img/OP.jpg">
+            <img src="media/img/DBZ.png">
+            <img src="media/img/OP.jpg">
+        </div>
+    </div>
+    <div class='home-manga-box'>
+        <span class='text-manga-box'>Top manga à lire</span>
+            <div class='top-scan-box'>
+                <?php 
+                    while($list = mysqli_fetch_assoc($resultat))
+                    {
+                        echo"
+                            <a href=scan.php?mid=".$list["manga_id"]." class='boxmanga'>
+                                <img src='media/aff_princi/".$list["manga_img"]."' class='pp'>
+                                <span>".$list["manga_nom"]."</span>
+                            </a>"
+                        ;
+                    }
+                ?>
+            </div>
+        <a href="manga.php" class='suite-manga'>
+            <p class='text_suite'>Accedez aux autres manga >>></p>
+        </a>
+    </div>
+</body>
+</html>
